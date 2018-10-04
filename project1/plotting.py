906def769e9282c1e05c5af29f5d426430659938
Halvard Sutterud <halvard.sutterud@gmail.com>
2018-10-04 22:53:35 +0200

    small changes and letter-adding helper function

--- project1/plotting.py
+++ project1/plotting.py
@@ -1,9 +1,12 @@
+"""
+Examples on how to plot the data using the Regression functions
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import tools
 from franke import FrankeFunction, FrankePlot
 
-def contour_plot(regr, N = 400, plot_err = True):
+def contour_plot(regr, N = 400, plot_err = True, deg=5):
     if plot_err:
         fig, axes = plt.subplots(1,2, figsize=[11,5])
     else:
@@ -25,7 +28,7 @@ def contour_plot(regr, N = 400, plot_err = True):
 
     zlin = zmesh.ravel()
 
-    X = tools.get_X_poly2D(xmesh.ravel(), ymesh.ravel(), deg = 5)
+    X = tools.get_X_poly2D(xmesh.ravel(), ymesh.ravel(), deg = deg)
     zhat = regr.predict(X)
 
     m = axes[0].imshow(zhat.reshape(N,N), extent=[0,1,0,1], origin='lower')
@@ -57,7 +60,7 @@ def plot_data_3D(x,y,z,zhat,beta):
     for i in range(x.size):
         ax.plot([x[i],x[i]],
                 [y[i],y[i]],
-                [z[i],zhat[i]], c = 'r', lw = 0.5, zorder = 10)
+                [zhat[i],FrankeFunction(x[i],y[i])], c = 'r', lw = 0.5, zorder = 10)
     ax.scatter(x,y,zhat)
 
     ax.set_xlabel('$x$')
@@ -77,12 +80,21 @@ def plot_covar(regr, deg=5):
     m = ax.imshow(regr.betaVar, origin='upper')
 
     orders = list(range(deg+1))
-    ax.set_xticks(np.cumsum(orders))
+    ax.set_xticks(np.cumsum(orders)-0.5)
     ax.set_xticklabels(orders)
 
-    ax.set_yticks(np.cumsum(orders))
+    ax.set_yticks(np.cumsum(orders)-0.5)
     ax.set_yticklabels(orders)
     ax.xaxis.set_ticks_position('top')
 
     plt.colorbar(m)
    
+
+from itertools import repeat
+def add_letters_to_axes(axes, letters = None, pos = repeat([0.01,0.9])):
+    if not letters:
+        letters = (chr(i) for i in range(65,91)) # large letters A-Z
+
+    for ax, l, [x,y] in zip(axes, letters, pos):
+        print(x,y)
+        ax.text(x,y,l,transform = ax.transAxes)
