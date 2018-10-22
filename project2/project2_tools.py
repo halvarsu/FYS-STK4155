03c6a268d997e1f0b68e6560eda2a7a521e1bf32
Halvard Sutterud <halvard.sutterud@gmail.com>
2018-10-22 16:10:24 +0200

    add 2D data

---
+++ project2/project2_tools.py
@@ -0,0 +1,5 @@
+import pickle
+
+def read_t(t,root="./data/IsingData/"):
+    data = pickle.load(open(root+'Ising2DFM_reSample_L40_T=%.2f.pkl'%t,'rb'))
+    return np.unpackbits(data).astype(int).reshape(-1,1600)
