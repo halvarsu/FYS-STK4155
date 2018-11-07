07bc7ec708bb57c40844a15e398e62c0c586108b
Heine Olsson Aabø <heine2307@1x-193-157-242-1.uio.no>
2018-11-07 19:24:28 +0100

    update ising notebooks

--- project2/logreg.py
+++ project2/logreg.py
@@ -1,9 +1,16 @@
 import numpy as np
 
 class Logistic_Regression:
-    def __init__(self, learn_rate=0.01, iterations=1000):
+    """
+    Logistic regression:
+    - Normalize => probabilities
+    - lmbd if regularization (L2)
+    """
+    def __init__(self, learn_rate=0.01, iterations=1000, normalize=False, lmbd=0):
         self.learn_rate = learn_rate
         self.iterations = iterations
+        self.normalize = normalize
+        self.lmbd = lmbd
     
     def fit(self, X, y):  
         self.beta = np.zeros(X.shape[1])
@@ -11,7 +18,10 @@ class Logistic_Regression:
         for i in range(self.iterations):
             z = np.dot(X, self.beta)
             p = self.sigmoid(z)
-            grad = (X.T @ (p - y)) #/ y.shape[0]
+            if self.normalize:
+                grad = (X.T @ (p - y) - self.lmbd*self.beta) / y.shape[0]
+            else:
+                grad = (X.T @ (p - y)) - self.lmbd*self.beta
             self.beta -= self.learn_rate * grad
     
     def predict(self, X, binary=True):
@@ -21,11 +31,14 @@ class Logistic_Regression:
         else:
             return self.ypred
     
-    def accuracy(self, y):
-        return np.sum(np.isclose(self.ypred,y))
+    def accuracy(self, y, y2):
+        if y.shape[0] == y2.shape[0]:
+            return np.sum(np.isclose(y,y2)) / y.shape[0]
+        else: 
+            raise ValueError('Arrays must have the same size')
     
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
     
     def cost(self, p, y):
-        return (-y * np.log(p) - (1 - y) * np.log(1 - p))
+        return (-y * np.log(p) - (1 - y) * np.log(1 - p) - 0.5*self.lmbd*(self.beta**2))
